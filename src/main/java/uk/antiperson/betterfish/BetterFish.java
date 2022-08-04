e833055ba98488d9111b876213526b1b3b32417c
Nathat23 <nathat890@outlook.com>
2022-08-04 23:22:15 +0100

    Add command

--- src/main/java/uk/antiperson/betterfish/BetterFish.java
+++ src/main/java/uk/antiperson/betterfish/BetterFish.java
@@ -1,8 +1,12 @@
 package uk.antiperson.betterfish;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
-public final class BetterFish extends JavaPlugin {
+public final class BetterFish extends JavaPlugin implements CommandExecutor {
 
     private FishingManager fishingManager;
 
@@ -12,6 +16,7 @@ public final class BetterFish extends JavaPlugin {
         // Plugin startup logic
         getServer().getPluginManager().registerEvents(new Listeners(this), this);
         new FishingTask(this).runTaskTimer(this,1,1);
+        getCommand("betterfish").setExecutor(this);
     }
 
     @Override
@@ -21,5 +26,13 @@ public final class BetterFish extends JavaPlugin {
 
     public FishingManager getFishingManager() {
         return fishingManager;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        sender.sendMessage("BetterFish v" + getDescription().getVersion());
+        sender.sendMessage("GitHub: https://github.com/Nathat23/BetterFish");
+        sender.sendMessage("Discord: https://discord.gg/GadyA9j");
+        return false;
     }
 }
